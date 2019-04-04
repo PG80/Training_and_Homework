15571d73d1ec6112f36a0d4bdc4c0d82a3336dab
PG <47541034+PG80@users.noreply.github.com>
2019-04-04 18:16:19 +0300

    Add files via upload

---
+++ 2018_12_14_TASK_1_PG_My_first_Calcilator.py
@@ -0,0 +1,14 @@
+# 2018.12.14. TASK_1_PG_My_first_Calcilator
+
+a = float(input())
+b = input()
+c = float(input())
+
+if b == '+':
+   print(a + c)
+if b == '-':
+   print(a - c)
+if b == '*':
+   print(a * c)
+if b == '/' and c == 0:
+   print(a / c)
